3374f803e408ed7b7eca0b2525fd2edd068cdfb3
yapeschock <student.jp@youngsvilleacademy.org>
2026-04-27 08:47:26 -0400

    Update headerinjections.js

--- headerinjections.js
+++ headerinjections.js
@@ -1,7 +1,7 @@
 let elem = document.createElement("div");
 document.body.prepend(elem);
-elem.innerHTML = "<link rel= 'stylesheet' href = 'http://yapeschock.github.io/web-dev/globalheader.html'"
-fetch("http://yapeschock.github.io/web-dev/header.css")  
+elem.innerHTML = "<link rel= 'stylesheet' href = 'https://yapeschock.github.io/web-dev/globalheader.html'"
+fetch("https://yapeschock.github.io/web-dev/header.css")  
   .then((output)=>output.text)
   .then((text)=>{elem.innerHTML = elem.innerHTML + text;})
   .catch((e)=>console.error(e));
