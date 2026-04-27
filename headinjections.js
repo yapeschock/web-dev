1b25a914801d87e01a8fe578f460e1488de7a4ab
yapeschock <student.jp@youngsvilleacademy.org>
2026-04-27 08:29:23 -0400

    Create headinjections.js

---
+++ headinjections.js
@@ -0,0 +1,7 @@
+let elem = document.createElement("div");
+document.body.prepend(elem);
+elem.innerHTML = "<link rel= 'stylesheet' href = 'http://yapeschock.github.io/web-dev/globalheader.html'"
+fetch("http://yapeschock.github.io/web-dev/header.css")  
+  .then((output)=>output.text)
+  .then((text)=>{elem.innerHTML=text;})
+  .catch((e)=>console.error(e));
